0a186a35481ecae9128a107c79b2079c1e86e374
Chizy <chizy7@gmail.com>
2021-02-17 20:34:35 -0500

    Added tab bar controller setup, collection view setup, collection view setup

---
+++ flixterhd/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  flixterhd
+//
+//  Created by Chizaram Chibueze on 2/17/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
